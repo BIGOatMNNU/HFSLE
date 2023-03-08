a47e25c9f98d24f6cba54665b6fe7e874b2d1689
BIGO <bigo_mnnu@163.com>
2023-03-08 15:28:33 +0800

    Update mainKNN.m

--- mainKNN.m
+++ mainKNN.m
@@ -3,14 +3,14 @@ str1={'DD';'F194'};
 alpha=[10 10];%α
 theta=[100 10];%θ
 beta=[10 0.01];%β
-m = length(str1); 
+m = length(str1);
 rng('default');
 for i = 1:m
     filename = [str1{i} 'Train']
     load (filename);
     [X,Y,Z]=create_SubTable2(data_array, tree);
     tic;
-    [feature{i},W{i},YD{i}] = HFSLE(X, Y,Z, tree, 10, alpha, theta ,beta,0);
+    [feature{i},W{i},YD{i}] = HFSLE(X, Y, Z, tree, 10, alpha(i), theta(i), beta(i), 0);
     TrainTime{i} =toc;
     %Test feature batch
     testFile = [str1{i}, 'Test.mat']
